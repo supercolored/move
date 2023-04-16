1c5b111d2b4c34e92f6b935c01e0ff96a2931940
Dennis Porter Jr <dennisporterjr@Denniss-MacBook-Pro.local>
2023-04-16 15:51:28 -0400

    adding demo

--- demo_sketch/sketch_move_demo/sketch_move_demo.js
+++ demo_sketch/sketch_move_demo/sketch_move_demo.js
@@ -1,63 +1,30 @@
-/**
- * @fileoverview Move p5.js library
- * @version 1.0.0
- * @license MIT
- * @author Dennis "Mars" Porter Jr
- * 
- * @see {@link https://github.com/supercolored/move}
- * 
- * @description
- * Move is a library for p5.js that provides a simple way to move objects around the canvas.
- * 
- * @example
- * const move = new Move();
- * 
- * function setup() {
- *  createCanvas(400, 400);
- * }
- * 
- * function draw() {
- *  background(220);
- * 
- *  // move between 0 and 100
- *  const x = move.between(0, 100, 0.5);
- * 
- *  // move around 0, 0
- *  const y = move.around(0, 0, 0, 0, Math.PI / 2);
- * 
- *  // move flock 0, 0
- *  const z = move.flock(0, 0, 0, 0, Math.PI / 2, 100);
- * }
- * 
- */
-
 const MOVE_EASING = {
-  /* @param {number} t - time */
-  quadIn: (t) => t * t,
-  quadOut: (t) => t * (2 - t),
-  quadInOut: (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
-  cubicIn: (t) => t * t * t,
-  cubicOut: (t) => (--t) * t * t + 1,
-  cubicInOut: (t) => t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
-  quartIn: (t) => t * t * t * t,
-  quartOut: (t) => 1 - (--t) * t * t * t,
-  quartInOut: (t) => t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t,
-  quintIn: (t) => t * t * t * t * t,
-  quintOut: (t) => 1 + (--t) * t * t * t * t,
-  quintInOut: (t) => t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t,
-  sineIn: (t) => 1 + Math.sin(Math.PI / 2 * t - Math.PI / 2),
-  sineOut: (t) => Math.sin(Math.PI / 2 * t),
-  sineInOut: (t) => 0.5 * (1 + Math.sin(Math.PI * t - Math.PI / 2)),
-  sexticIn: (t) => t * t * t * t * t * t,
-  sexticOut: (t) => 1 - (1 - t) * (1 - t) * (1 - t) * (1 - t) * (1 - t) * (1 - t),
-  sexticInOut: (t) => t < 0.5 ? 6 * t * t * t * t * t * t : 1 - Math.pow(-2 * t + 2, 6) / 2,
-}
+};
 
 class Move {
   constructor() {
-    this.pace = MOVE_EASING;
+    this.pace = {
+      /* @param {number} t - time */
+      quadIn: (t) => t * t,
+      quadOut: (t) => t * (2 - t),
+      quadInOut: (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
+      cubicIn: (t) => t * t * t,
+      cubicOut: (t) => (--t) * t * t + 1,
+      cubicInOut: (t) => t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+      quartIn: (t) => t * t * t * t,
+      quartOut: (t) => 1 - (--t) * t * t * t,
+      quartInOut: (t) => t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t,
+      quintIn: (t) => t * t * t * t * t,
+      quintOut: (t) => 1 + (--t) * t * t * t * t,
+      quintInOut: (t) => t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t,
+      sineIn: (t) => 1 + Math.sin(Math.PI / 2 * t - Math.PI / 2),
+      sineOut: (t) => Math.sin(Math.PI / 2 * t),
+      sineInOut: (t) => 0.5 * (1 + Math.sin(Math.PI * t - Math.PI / 2)),
+      sexticIn: (t) => t * t * t * t * t * t,
+      sexticOut: (t) => 1 - (1 - t) * (1 - t) * (1 - t) * (1 - t) * (1 - t) * (1 - t),
+      sexticInOut: (t) => t < 0.5 ? 6 * t * t * t * t * t * t : 1 - Math.pow(-2 * t + 2, 6) / 2,
+    };
   }
-
   /** 
    * @param {number} start - start value
    * @param {number} end - end value
@@ -142,4 +109,17 @@ class Move {
   };
 }
 
-export default Move;
+const move = new Move();
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+function draw() {
+  background(0);
+
+  const { x, y } = move.around(mouseX, mouseY, width / 2, height / 2, frameCount / 100);
+  ellipse(x, y, 50, 50);
+}
